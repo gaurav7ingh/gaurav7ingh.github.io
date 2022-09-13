f36ecb163268afe3ba0f2654614c40f8d67fc0c4
gaurav7ingh <gauravsingh99626@gmail.com>
2022-09-13 17:48:16 +0530

    done by gaurav

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ window.onscroll = function () {
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("nav").style.top = "0";
         document.getElementById("right").style.top = "100";
+        document.getElementById("ham").style.top = "0";
     } else {
         document.getElementById("nav").style.top = "-100px";
         document.getElementById("right").style.top = "0";
+        document.getElementById("ham").style.top = "-100";
     }
     prevScrollpos = currentScrollPos;
 }
